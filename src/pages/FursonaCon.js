a79089d4542ced3f1d0aeb5468bba2643ee34132
Trevor Welch <delaxiox@gmail.com>
2025-08-29 18:45:35 -0400

    Security Fix

--- src/pages/FursonaCon.js
+++ src/pages/FursonaCon.js
@@ -11,7 +11,7 @@ function FursonaCon() {
     <h1 className="center-p">FursonaCon 2025 Hidden Events</h1>
     <br/>
     <h2 className="center-p">Dealer's Den Stamp Rally</h2>
-    <a target="_blank" href={stampRally}><img src={stampRally} className="stampRally"/></a>
+    <a target="_blank" rel="noreferrer"  href={stampRally}><img src={stampRally} alt="stamp rally graphic" className="stampRally"/></a>
     <br/>
     <h2 className="center-p">Attendee Hosted Events</h2>
     <br/>
