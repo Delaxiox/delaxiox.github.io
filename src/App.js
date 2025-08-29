3b8cea7a2bf458efe840406c38bb2f57c3b620f9
Trevor Welch <delaxiox@gmail.com>
2025-08-29 18:07:39 -0400

    Hash Routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter, HashRouter, Routes, Route } from 'react-router-dom';
+import React from 'react';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Layout from './layouts/Layout';
 import FullPageLayout from './layouts/FullPageLayout';
 import Home from './pages/Home';
@@ -10,36 +10,17 @@ import NotFound from './pages/NotFound';
 import FursonaCon from './pages/FursonaCon';
 
 function App() {
-  const [useHash, setUseHash] = useState(false);
-
-  useEffect(() => {
-    // If GitHub Pages serves 404, fallback to hash routing
-    // Detect by checking if current path !== "/" and no matching file exists
-    const path = window.location.pathname;
-    if (path !== '/' && !path.includes('.')) {
-      fetch(path)
-        .then(response => {
-          if (response.status === 404) {
-            setUseHash(true);
-          }
-        })
-        .catch(() => setUseHash(true));
-    }
-  }, []);
-
-  const Router = useHash ? HashRouter : BrowserRouter;
-
   return (
-    <Router>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Layout><Home/></Layout>} />
         <Route path="/about" element={<Layout><About/></Layout>} />
         <Route path="/contact" element={<Layout><Contact/></Layout>} />
         <Route path="/credits" element={<Layout><Credits/></Layout>} />
         <Route path="/fursonacon" element={<FullPageLayout><FursonaCon/></FullPageLayout>} />
-        <Route path="*" element={<Layout><NotFound/></Layout>} />  
+        <Route path="*" element={<Layout><NotFound/></Layout>} />
       </Routes>
-    </Router>
+    </HashRouter>
   );
 }
 
